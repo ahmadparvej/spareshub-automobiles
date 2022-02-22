0eea5d633545327647667955efd8140818f94edd
Parvej Ahmad <ahmadparvej58@gmail.com>
2022-02-22 19:25:38 +0530

    commit by parvej
    products

---
+++ js/productData.js
@@ -0,0 +1,34 @@
+var productData = [
+    {
+        image: 'https://spareshub.com/pub/media/catalog/product/placeholder/default/xno_image_3.png.pagespeed.ic.CL0sy0K3dL.webp',
+        title: 'Fuse Box Assembly for TAT ACE BS-II 06-07 MDL-(282954600121)',
+        price:'₹495.00'
+    },
+    {
+        image: 'https://spareshub.com/pub/media/catalog/product/placeholder/default/xno_image_3.png.pagespeed.ic.CL0sy0K3dL.webp',
+        title: 'Fuse Box Assembly for TAT ACE BS-II 06-07 MDL-(282954600121)',
+        price:'₹495.00'
+    },
+    {
+        image: 'https://spareshub.com/pub/media/catalog/product/placeholder/default/xno_image_3.png.pagespeed.ic.CL0sy0K3dL.webp',
+        title: 'Fuse Box Assembly for TAT ACE BS-II 06-07 MDL-(282954600121)',
+        price:'₹495.00'
+    },
+    {
+        image: 'https://spareshub.com/pub/media/catalog/product/placeholder/default/xno_image_3.png.pagespeed.ic.CL0sy0K3dL.webp',
+        title: 'Fuse Box Assembly for TAT ACE BS-II 06-07 MDL-(282954600121)',
+        price:'₹495.00'
+    },
+    {
+        image: 'https://spareshub.com/pub/media/catalog/product/placeholder/default/xno_image_3.png.pagespeed.ic.CL0sy0K3dL.webp',
+        title: 'Fuse Box Assembly for TAT ACE BS-II 06-07 MDL-(282954600121)',
+        price:'₹495.00'
+    },
+    {
+        image: 'https://spareshub.com/pub/media/catalog/product/placeholder/default/xno_image_3.png.pagespeed.ic.CL0sy0K3dL.webp',
+        title: 'Fuse Box Assembly for TAT ACE BS-II 06-07 MDL-(282954600121)',
+        price:'₹495.00'
+    },
+]
+
+localStorage.setItem("productData",JSON.stringify(productData));
